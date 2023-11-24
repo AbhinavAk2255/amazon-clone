0884811064420c31e32bc192c89172f9d56a36de
Abhinav Ak <abhinavkukku48@gmail.com>
2023-11-24 15:16:26 +0530

    starting page creation

---
+++ script.js
@@ -0,0 +1,66 @@
+const product = [{
+    Image: 'images/athletic-cotton-socks-6-pairs.jpg',
+    nmae : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating : {
+        star : 4.5,
+        count : 57
+    },
+    priceCents : 1090
+},{
+    Image: 'images/intermediate-composite-basketball.jpg',
+    nmae : 'Intermediate Size Basketball',
+    rating : {
+        star : 4,
+        count : 127
+    },
+    priceCents : 2095
+},{
+    Image: 'images/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    nmae : 'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating : {
+        star : 4.5,
+        count : 56
+    },
+    priceCents : 799
+},{
+    Image: 'images/black-2-slot-toaster.jpg',
+    nmae : 'Slot Toaster - Black',
+    rating : {
+        star : 5,
+        count : 2197
+    },
+    priceCents : 1899
+},{
+    Image: 'images/6-piece-white-dinner-plate-set.jpg',
+    nmae : '6 Piece White Dinner Plate Set',
+    rating : {
+        star : 4,
+        count : 37
+    },
+    priceCents : 2067
+},{
+    Image: 'images/6-piece-non-stick-baking-set.webp',
+    nmae : '6-Piece Nonstick, Carbon Steel Oven Bakeware Baking',
+    rating : {
+        star : 4.5,
+        count : 175
+    },
+    priceCents : 3499
+},{
+    Image: 'images/plain-hooded-fleece-sweatshirt-yellow.jpg',
+    nmae : 'Plain Hooded Fleece Sweatshirt',
+    rating : {
+        star : 4.5,
+        count : 317
+    },
+    priceCents : 2400,
+    button_color : {
+        color1 : 'Yellow',
+        color2 : 'Teal'
+    },
+    size_button : {
+        small : 'S',
+        medium : 'M',
+        large : 'L'
+    },
+}]
