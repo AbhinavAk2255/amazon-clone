936a3925126019e32c4442007f10aeaca1a11516
Abhinav Ak <abhinavkukku48@gmail.com>
2023-11-24 18:05:43 +0530

    html content added using js loop

--- script.js
+++ script.js
@@ -1,66 +1,48 @@
-const product = [{
-    Image: 'images/athletic-cotton-socks-6-pairs.jpg',
-    nmae : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating : {
-        star : 4.5,
-        count : 57
-    },
-    priceCents : 1090
-},{
-    Image: 'images/intermediate-composite-basketball.jpg',
-    nmae : 'Intermediate Size Basketball',
-    rating : {
-        star : 4,
-        count : 127
-    },
-    priceCents : 2095
-},{
-    Image: 'images/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    nmae : 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating : {
-        star : 4.5,
-        count : 56
-    },
-    priceCents : 799
-},{
-    Image: 'images/black-2-slot-toaster.jpg',
-    nmae : 'Slot Toaster - Black',
-    rating : {
-        star : 5,
-        count : 2197
-    },
-    priceCents : 1899
-},{
-    Image: 'images/6-piece-white-dinner-plate-set.jpg',
-    nmae : '6 Piece White Dinner Plate Set',
-    rating : {
-        star : 4,
-        count : 37
-    },
-    priceCents : 2067
-},{
-    Image: 'images/6-piece-non-stick-baking-set.webp',
-    nmae : '6-Piece Nonstick, Carbon Steel Oven Bakeware Baking',
-    rating : {
-        star : 4.5,
-        count : 175
-    },
-    priceCents : 3499
-},{
-    Image: 'images/plain-hooded-fleece-sweatshirt-yellow.jpg',
-    nmae : 'Plain Hooded Fleece Sweatshirt',
-    rating : {
-        star : 4.5,
-        count : 317
-    },
-    priceCents : 2400,
-    button_color : {
-        color1 : 'Yellow',
-        color2 : 'Teal'
-    },
-    size_button : {
-        small : 'S',
-        medium : 'M',
-        large : 'L'
-    },
-}]
+
+let productHtml = '';
+
+products.forEach((product) => {
+    productHtml += `
+
+    <div class="product-container">
+        <div class="image-area">
+            <img class="container-image" src="${product.image}" alt="">
+        </div>
+        <div class="product-para">
+            ${product.name}
+        </div>
+        <div class="rating">
+            <img src="rating-images/rating-${product.rating.stars*10}.png" alt="">
+            <span>${product.rating.count}</span>
+        </div>
+        <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
+        </div>
+        <div class="quantity">
+            <select class="selection-item" name="" id="">
+                <option selected value="1">1</option>
+                <option  value="2">2</option>
+                <option  value="3">3</option>
+                <option  value="4">4</option>
+                <option  value="5">5</option>
+                <option  value="6">6</option>
+                <option  value="7">7</option>
+                <option  value="8">8</option>
+                <option  value="9">9</option>
+                <option  value="10">10</option>
+            </select>
+        </div>
+
+
+        <div class="product-spacer"></div>
+        <div class="added-message">
+            <img src="/icons/checkmark.png/" alt="">
+            Added
+        </div>
+        <button class="add-btn">Add to cart</button>
+    </div>
+    `
+})
+
+
+document.querySelector('.js-html-space').innerHTML = productHtml;
